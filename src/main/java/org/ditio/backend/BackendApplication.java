3ad6f107c88e870be09135327a9c5d7bb3f25d92
80bble <gitta98@ymail.com>
2026-02-27 10:36:57 +0100

    Working on flyway

--- src/main/java/org/ditio/backend/BackendApplication.java
+++ src/main/java/org/ditio/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package org.ditio.backend;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,20 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		
+		
+		
+		String Dburl  = System.getenv("SPRING_DATASOURCE_URL");
+		String username = System.getenv("POSTGRES_USER");
+		String password = System.getenv("POSTGRES_PASSWORD");
+
+		Flyway flyway = Flyway.configure()
+		.dataSource(Dburl, username, password)
+		.load();
+		
+		// Start the migration
+		flyway.migrate();
 	}
+	
 
 }
