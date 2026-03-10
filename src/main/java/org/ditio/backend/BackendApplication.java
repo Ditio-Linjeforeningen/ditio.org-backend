189c56173ebecdf63a2fb3ba3b572b64b40de991
80bble <gitta98@ymail.com>
2026-03-10 14:35:23 +0100

    Fikset flyway exceptions

--- src/main/java/org/ditio/backend/BackendApplication.java
+++ src/main/java/org/ditio/backend/BackendApplication.java
@@ -11,18 +11,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		
-		
-		
-		String Dburl  = System.getenv("SPRING_DATASOURCE_URL");
-		String username = System.getenv("POSTGRES_USER");
-		String password = System.getenv("POSTGRES_PASSWORD");
-
-		Flyway flyway = Flyway.configure()
-		.dataSource(Dburl, username, password)
-		.load();
-		
-		// Start the migration
-		flyway.migrate();
 	}
 	
 
