eca3446f64d60d98dbad641a8d83b3ec20b963fd
80bble <gitta98@ymail.com>
2026-03-16 18:27:36 +0100

    testforsøk otp+put sammen

--- src/main/java/org/ditio/backend/Controllers/TestAttController.java
+++ src/main/java/org/ditio/backend/Controllers/TestAttController.java
@@ -1,15 +1,21 @@
 
 package org.ditio.backend.Controllers;
 
+import org.ditio.backend.TimeBasedOnetimePassword;
 import org.ditio.backend.Entities.TestAtt;
 import org.ditio.backend.Repositories.TestAttRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -18,9 +24,18 @@ import java.util.UUID;
 public class TestAttController {
 
     private final TestAttRepository repository;
+     private static final int STEP_SECONDS = 30;
 
-    public TestAttController(TestAttRepository repository) {
+    private final String secretBase32;
+    private final Clock clock = Clock.systemUTC();
+
+    public TestAttController(TestAttRepository repository, 
+        @Value("${otp.secret:OTP_CONFIG}") String secretConfig,
+        @Value("${otp.secret.isBase32:OTP_CONFIG_STATUS}") boolean isBase32) {
         this.repository = repository;
+         this.secretBase32 = isBase32
+                ? secretConfig
+                : TimeBasedOnetimePassword.encodeBase32(secretConfig);
     }
 
     // GET all items
@@ -72,5 +87,46 @@ public class TestAttController {
            return ResponseEntity.ok(testAtt);
         
         }
+
+//OTP
+@GetMapping("/current")
+    public Map<String, Object> current() {
+        long now = Instant.now(clock).getEpochSecond();
+        String code = TimeBasedOnetimePassword.generateTOTP(secretBase32);
+
+        long remaining = STEP_SECONDS - (now % STEP_SECONDS);
+        Instant expiresAt = Instant.ofEpochSecond(now + remaining);
+
+        return Map.of("code", code, "expiresAt", expiresAt.toString());
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<Map<String, Object>> verify(@RequestBody Map<String, String> body) {
+        String input = body.getOrDefault("code", "").trim();
+        boolean valid = TimeBasedOnetimePassword.validateTOTP(secretBase32, input);
+        return ResponseEntity.ok(Map.of("true", valid));
+    }
+
+    //Forsøk på å kombinere OTP validering og PUT endring av status 
+    //https://spring.io/guides/tutorials/rest
+    @PutMapping("/verify2/{id}")
+    Optional<TestAtt> updateTestAtt(@RequestBody TestAtt testAtta, @RequestBody Map<String, String>body, @PathVariable UUID id) {
+        String input = body.getOrDefault("code", "").trim();
+
+        boolean valid = TimeBasedOnetimePassword.validateTOTP(secretBase32, input);
+        if(valid == true){
+            System.out.print("Input og kode matcher");
+
+            return repository.findById(id)
+            .map(testAtt-> {
+                testAtt.setAtt_status(testAtta.getAtt_status());
+                return repository.save(testAtt);
+            });
+        }
+        else{
+            //System.out.print("Input og kode matcher IKKE");
+            return null;
+        }
+    }
 
 }
