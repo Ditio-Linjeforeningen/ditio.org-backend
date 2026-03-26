14c940c3b7606bb09c2b316cb1a41d524f33be71
jacobdm98 <jacobdahlmarcussen@gmail.com>
2026-03-26 13:49:59 +0100

    meg endpoint

--- src/main/java/org/ditio/backend/Controllers/UserController.java
+++ src/main/java/org/ditio/backend/Controllers/UserController.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.ditio.backend.Entities.User;
 import org.ditio.backend.Repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +36,15 @@ public class UserController {
     @GetMapping("/all")
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/meg")
+    public ResponseEntity<User> getMyInfo(@AuthenticationPrincipal OidcUser principal) {
+    // Henter ID fra token
+        String myId = principal.getSubject(); 
+    
+        return userRepository.findById(myId)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
 }
