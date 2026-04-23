3bac058ff94b072618d23c29bfc060d3a0dfe106
jacobdm98 <jacobdahlmarcussen@gmail.com>
2026-04-23 14:38:55 +0200

    refaktoringen, og mere oversigtelig kode

--- src/main/java/org/ditio/backend/FeideUser/SecurityConfig.java
+++ src/main/java/org/ditio/backend/FeideUser/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 // Event CRUD kræver admin rettigheder
                 .requestMatchers("/events/**").hasAnyRole("ADMIN", "SUPER_ADMIN")
 
-                // Kun for test 
+                // Kun for test     
                 .requestMatchers("/feide/test").authenticated()
 
                 //Bare superadmin kan ændre roller
@@ -39,11 +39,15 @@ public class SecurityConfig {
                 // Resten er åbent for alle
                 .anyRequest().permitAll()  
             )
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/feide/test", true) //OAuth2 Login
-           .redirectionEndpoint(redirection -> redirection
-                .baseUri("/login/callback") 
-            )
-            .defaultSuccessUrl("/feide/test", true)
+                .oauth2Login(oauth2 -> oauth2
+                //Synkroniserer med UserDB service
+                .userInfoEndpoint(userInfo -> userInfo.oidcUserService(userDB))
+                //henter token hos feide
+                .redirectionEndpoint(redirection -> redirection.baseUri("/login/callback"))
+                //Går på default til /api/users/meg efter login
+                //Front end burde kunne overstyre denne her, 
+                // hvis de vil sende brugeren tilbage hvor den kom fra efter indlogging
+                .defaultSuccessUrl("/api/users/meg", true)
         );
 
         return http.build();
